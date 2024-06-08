318bb1d719e4bc38f1208ccebdc060c599f7e339
Tianqin Li <jacklitianqin@gmail.com>
2024-06-08 00:26:58 -0700

    add copy key

--- copy-github-key-to-server.sh
+++ copy-github-key-to-server.sh
@@ -6,20 +6,24 @@ read -p "Enter your username: " username
 # Prompt for the server address
 read -p "Enter the server address: " server_address
 
+# Prompt for the port
+read -p "Enter the port: " port
+
 # Display the entered information
 echo "Username: $username"
 echo "Server Address: $server_address"
+echo "Port: $port"
 
 echo "Copying keys..."
 echo "[1] RUN scp ~/.ssh/id_rsa_server.pub $username@$server_address:/home/$username/.ssh/id_rsa_server.pub"
 
 server_home_ssh_dir_cmd="mkdir -p \$HOME/.ssh/"
-ssh $username@$server_address "bash -c '${server_home_ssh_dir_cmd}'"
+ssh -p $port $username@$server_address "bash -c '${server_home_ssh_dir_cmd}'"
 
-scp ~/.ssh/id_rsa_server.pub $username@$server_address:~/.ssh/
+scp -P $port ~/.ssh/id_rsa_server.pub $username@$server_address:~/.ssh/
 
 echo "[2] RUN scp ~/.ssh/id_rsa_server $username@$server_address:~/.ssh/id_rsa_server"
-scp ~/.ssh/id_rsa_server $username@$server_address:~/.ssh/
+scp -P $port ~/.ssh/id_rsa_server $username@$server_address:~/.ssh/
 
 echo "[3] Configuring server..."
-ssh $username@$server_address 'bash -s' < server_configure.sh
+ssh -p $port $username@$server_address 'bash -s' < server_configure.sh
